470bbeaeb294875e0223f152ac5e216ff9a37936
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-08-11 09:13:00 +0300

    simplification

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
 
         var map = L.map('map', { 
           zoomControl: true,
-          center: [58.2527, 22.4872],
+          center: [58.2521, 22.4854],
           zoom: 16,
           attributionControl: false
         });
@@ -22,83 +22,47 @@
         L.tileLayer('http://a.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png', {
           attribution: 'Mapbox'
         }).addTo(map);
-
+//#998f84
         cartodb.createLayer(map, {
               user_name: 'kika',
               type: 'cartodb',
               sublayers: [
-              {
-                sql: '\
-                SELECT\
-                  l.*,\
-                  a.selgitus,\
-                  a.hoone_funk\
-                FROM\
-                  linna_majad_2 AS l\
-                JOIN ajaloolised_hooned_2 AS a ON (l.aadress = a.aadress)\
-                WHERE\
-                 ST_Intersects(l.the_geom,a.the_geom)\
-                ',
-                cartocss: '#layer { polygon-fill:#998f84; polygon-opacity: 1; line-opacity:0; line-color: #000;}',
-//                interactivity: ['aadress','foto_pikk','ehit_aasta','seisukord','selgitus','hoone_funk']
-              },   
-              {
-                sql: '\
-                SELECT\
-                  l.*,\
-                  w.text,\
-                  w.url\
-                FROM\
-                  kuressaare_wikipedia_3 AS w,\
-                  linna_majad_2 AS l\
-                JOIN ajaloolised_hooned_2 AS a ON (l.aadress = a.aadress)\
-                WHERE\
-                 ST_Intersects(l.the_geom,w.the_geom)\
-                ',
-                cartocss: '#layer { polygon-fill:#544; polygon-opacity: 1; line-opacity:0; line-color: #000;}',
-//                interactivity: ['aadress','foto_pikk','ehit_aasta','seisukord','selgitus','hoone_funk','url','text']
-              },
-              {
+               {
                 sql: '\
                 SELECT\
                   l.*\
                 FROM\
                   linna_majad_2 AS l\
                 ',
-                cartocss: '#layer { polygon-fill: #998f84; polygon-opacity: 0.2; line-opacity:0; }',
+                cartocss: '#layer { polygon-fill: #998f84; polygon-opacity: 0.9; line-opacity:0; }',
                 interactivity: ['cartodb_id', 'aadress','foto_pikk','ehit_aasta','seisukord']
               },
-   /*         {
+              {
               sql: '\
               SELECT\
-                v.*\
+                w.*\
               FROM\
-                vanalinna_fotod AS v\
+                kuressaare_wikipedia_3 AS w\
               ',
-              cartocss: '#layer { marker-fill: #000; marker-opacity: 0.5; marker-line-color: #fff; marker-line-width: 0; marker-line-opacity: 0.5; marker-width: 9;  }',
-              interactivity: ['foto','aasta','asukoht','selgitus']
-              }              
-  */            ]
+              cartocss: '#layer { marker-width: 18; marker-file: url("http://upload.wikimedia.org/wikipedia/commons/8/8f/W-circle.svg"); }',              }              
+              ]
             },{cartodb_logo: false})
             .addTo(map)
             .done(function(layer) {
 
                 var sql = new cartodb.SQL({ user: 'kika' })
 
-                var sl2 = layer.getSubLayer(2);
+                var sl = layer.getSubLayer(0);
               
-                sl2.setInteraction(true);
+                sl.setInteraction(true);
               
-                sl2.on('featureClick', function(e, pos, pixel, data) {  
-                  if (data.ehit_aasta == 0) data.ehit_aasta = null        
-              
-              
+                sl.on('featureClick', function(e, pos, pixel, data) {  
+                  if (data.ehit_aasta == 0) data.ehit_aasta = null                      
                     sql.execute("SELECT * FROM vanalinna_fotod WHERE asukoht LIKE '%{{ aadress }}%'", { aadress: data.aadress })
                     .done(function(new_data) {
-                      console.log(new_data)
                       if (new_data.rows) {
                         data.fotod = new_data.rows.map(function(item) {
-                          item.selgitus = item.selgitus.replace(/Ã¤/g, 'ä').replace(/Ãµ/g, 'õ').replace(/Ã¼/g, 'ü').replace(/Ã¾/g, 'sh')
+                          item.selgitus = item.selgitus ? item.selgitus.replace(/Ã¤/g, 'ä').replace(/Ãµ/g, 'õ').replace(/Ã¼/g, 'ü').replace(/Ã¾/g, 'sh') : null
                           return item
                         })
                       }
