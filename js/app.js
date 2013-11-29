2c68996ac499cba40580dfe693257cfd9b9113cb
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-11-29 16:49:42 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -29,77 +29,82 @@ function main() {
       attribution: 'Mapbox'
     }).addTo(map);
 
-    cartodb.createLayer(map, {
-      user_name: 'kristjanjansen',
-      type: 'cartodb',
-      sublayers: [{
-        sql: 'SELECT * FROM buildings',
-        cartocss: '#layer { polygon-fill: #998f84; polygon-opacity: 0.9; line-opacity:0; }',
-        interactivity: ['cartodb_id', 'aadress', 'foto_pikk', 'ehit_aasta', 'seisukord']
-      }, {
-        sql: 'SELECT * FROM wikipedia',
-        cartocss: '#layer { marker-width: 18; marker-file: url("http://upload.wikimedia.org/wikipedia/commons/8/8f/W-circle.svg"); }'
-      }]
-    }, {
-      cartodb_logo: false
-    }).addTo(map).done(function(layer) {
-      var sl = layer.getSubLayer(0);
-      sl.setInteraction(true);
-      sl.on('featureClick', function(e, pos, pixel, data) {
-        getSidebar(data)
-      });
+    cartodb.createLayer(
+      map, 
+      'http://kristjanjansen.cartodb.com/api/v2/viz/3a8eff2e-58fd-11e3-b16b-a5971feaabf3/viz.json', 
+      {infowindow: true}
+      )
+            .addTo(map)
+            .on('done', function(layer) {
 
-/* 
-      var sl3 = layer.getSubLayer(3);
-      sl3.setInteraction(true);
-      sl3.on('featureClick', function(e, pos, pixel, data) {  
-        $('#sidebar').html(sidebar.render({data: data}))
-      });                                 
-*/
-
-    });
-
-    }
+             layer.setInteraction(true);
+             layer.setInteractivity(['cartodb_id', 'aadress']);
+             
+             var sublayer = layer.getSubLayer(0);
+              
+             layer.on('featureClick', function(e, pos, latlng, data) {
+               console.log(data)
+               getSidebar(data.cartodb_id);
+             });
+             
+      
+             layer.on('error', function(err) {
+               console.log('error: ' + err);
+             });
+             
+           }).on('error', function() {
+             console.log.log("some error occurred");
+           });
+         
+     }
     
     
-    
-    
-function getSidebar(data) {
+    function getSidebar(cartodb_id) {
 
-  if (data.ehit_aasta == 0) data.ehit_aasta = null
-  sql.execute("SELECT * FROM historic_photos WHERE asukoht LIKE '%{{ aadress }}%'", {
-    aadress: data.aadress
-  }).done(function(new_data) {
-    if (new_data.rows) {
-      data.fotod = new_data.rows.map(function(item) {
-        item.selgitus = item.selgitus ? item.selgitus.replace(/Ã¶/g, 'ö').replace(/Ã¤/g, 'ä').replace(/Ã/g, 'Ä').replace(/Ãµ/g, 'õ').replace(/Ã¼/g, 'ü').replace(/Ã¾/g, 'sh') : null
-        return item
-      })
-    }
-    sql.execute("SELECT * FROM historic_buildings WHERE aadress LIKE '%{{ aadress }}%'", {
-      aadress: data.aadress
-    }).done(function(new_data) {
-      if (new_data.rows[0]) {
-        data.selgitus = new_data.rows[0].selgitus
-        data.hoone_funk = new_data.rows[0].hoone_funk
-      }
-
-      sql.execute("SELECT b.cartodb_id, w.url, w.text FROM buildings AS b, wikipedia AS w WHERE b.cartodb_id = {{ id }} AND ST_Intersects(b.the_geom, w.the_geom)", {
-        id: data.cartodb_id
+      sql.execute("SELECT * FROM buildings WHERE cartodb_id = {{ cartodb_id }}", {
+        cartodb_id: cartodb_id
+      }).done(function(data) {
+        data = data.rows[0]
+        
+      if (data.ehit_aasta == 0) data.ehit_aasta = null
+    
+      sql.execute("SELECT * FROM historic_photos WHERE asukoht LIKE '%{{ aadress }}%'", {
+        aadress: data.aadress
       }).done(function(new_data) {
-        if (new_data.rows[0]) {
-          data.url = new_data.rows[0].url
-          data.text = new_data.rows[0].text
+        if (new_data.rows) {
+          data.fotod = new_data.rows.map(function(item) {
+            item.selgitus = item.selgitus ? item.selgitus.replace(/Ã¶/g, 'ö').replace(/Ã¤/g, 'ä').replace(/Ã/g, 'Ä').replace(/Ãµ/g, 'õ').replace(/Ã¼/g, 'ü').replace(/Ã¾/g, 'sh') : null
+            return item
+          })
         }
-        $('#sidebar').html(sidebar.render({
-          data: data
-        }))
+        sql.execute("SELECT * FROM historic_buildings WHERE aadress LIKE '%{{ aadress }}%'", {
+          aadress: data.aadress
+        }).done(function(new_data) {
+          if (new_data.rows[0]) {
+            data.selgitus = new_data.rows[0].selgitus
+            data.hoone_funk = new_data.rows[0].hoone_funk
+          }
+
+          sql.execute("SELECT b.cartodb_id, w.url, w.text FROM buildings AS b, wikipedia AS w WHERE b.cartodb_id = {{ id }} AND ST_Intersects(b.the_geom, w.the_geom)", {
+            id: data.cartodb_id
+          }).done(function(new_data) {
+            if (new_data.rows[0]) {
+              data.url = new_data.rows[0].url
+              data.text = new_data.rows[0].text
+            }
+            $('#sidebar').html(sidebar.render({
+              data: data
+            }))
+          })
+
+        })
+
       })
-
+      
     })
-
-  })
-}
+    
+  }
+    
 
 
 window.onload = main;
